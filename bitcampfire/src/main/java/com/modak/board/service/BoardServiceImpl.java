fa4b09e913a250d2f51007b34fdff81cb8e731f4
poonghyeok <vpd3302@naver.com>
2022-07-21 22:08:42 +0900

    관리자2차분류살리기

--- bitcampfire/src/main/java/com/modak/board/service/BoardServiceImpl.java
+++ bitcampfire/src/main/java/com/modak/board/service/BoardServiceImpl.java
@@ -856,6 +856,7 @@ public class BoardServiceImpl implements BoardService {
 			
 			session.invalidate();
     }
+}
 		
 	// @@@@@@@@@ 연수 끝: admincontroller > 어드민 페이지 > 공지사항 관리  @@@@@@@@@ 	
 	//<!--@@@@ 연수 살려주세요!(220721)  -->	
